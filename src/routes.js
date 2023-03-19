f523f071bb43076dd6c637e9341daceac7234ab9
Calvin Danyalson <calvindany1102@gmail.com>
2023-03-19 17:39:28 +0700

    Adding Detele Notes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler } = require("./handler");
-
+const { 
+    addNoteHandler, 
+    getAllNotesHandler, 
+    getNoteByIdHandler, 
+    editNoteByIdHandler, 
+    deleteNoteByIdHandler 
+} = require("./handler");
 
 const routes = [
     {
@@ -21,6 +26,11 @@ const routes = [
         method: 'PUT',
         path: '/notes/{id}',
         handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
     }
 ];
 
